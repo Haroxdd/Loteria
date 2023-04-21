2e9e5c9e29c87643288e4d105d5f2cb46558f87a
CocaDVidrio <131321073+CocaDVidrio@users.noreply.github.com>
2023-04-20 23:16:16 -0600

    Add files via upload

--- Carton.java
+++ Carton.java
@@ -7,12 +7,14 @@ import javax.swing.JPanel;
 
 public class Carton extends JPanel implements MouseListener, MouseMotionListener {
 Carta[] cartas = new Carta[16];
-String CartasJugadas ="";
+String CartasJugadas ="", cartasCantadas = "";
 GUI_Cliente pointer;
+
     
 public Carton(GUI_Cliente v){
     this.pointer = v;
     cartas[0] = new Carta(getClass().getResource("/1.png"), "1", 50, 0, this);
+    cartas[1] = new Carta(getClass().getResource("/2.png"), "2", 100, 0, this);
     this.addMouseListener(this);
     this.addMouseMotionListener(this);
 }
@@ -22,16 +24,29 @@ public Carton(GUI_Cliente v){
         g.setColor(Color.white);
        g.fillRect(0, 0, 300, 300);
        cartas[0].pintar(g);
+       cartas[1].pintar(g);
        repaint();
     }
-
+    
+    public void LanzamientoCliente(){
+   //     pointer.sc.enviarMensaje(CartasJugadas);
+    }
+    
+    public void RecibirMensaje(String mensaje){
+        cartasCantadas += mensaje;
+        System.out.println(cartasCantadas);
+    }
+    
     @Override
     public void mouseClicked(MouseEvent e) {
     //Aqui se colocan los 16 if de cada caso
         if(cartas[0].estaEnArea(e.getPoint()))
         cartas[0].lanzamiento(e.getPoint());
-    
+        
+        if(cartas[1].estaEnArea(e.getPoint()))
+        cartas[1].lanzamiento(e.getPoint());
     }
+    
     @Override
     public void mousePressed(MouseEvent e) {
         

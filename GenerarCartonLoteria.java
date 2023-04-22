3017ed42a94576cebc992cde31c887be5f619922
SNK1206 <105396545+SNK1206@users.noreply.github.com>
2023-04-21 23:08:13 -0600

    Add files via upload

--- GenerarCartonLoteria.java
+++ GenerarCartonLoteria.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class GenerarCartonLoteria extends JPanel implements MouseListener,MouseMotionListener{
     private Carta car1,car2,car3,car4,car5,car6,car7,car8,car9,car10,car11,car12,car13,car14,car15,car16;
     private int x,y,Cartas[]=new int[54],Carton[][]=new int[4][4],valorGenerado;
-    String CartasJugadas="";
+    String CartasJugadas="",cartasCantadas="";
     private Programa p;
     private JTextArea a;
     public GenerarCartonLoteria(Programa p){
@@ -33,7 +33,7 @@ public class GenerarCartonLoteria extends JPanel implements MouseListener,MouseM
             v[posAle]=temp;
         }
     }
-    public void Mostrar(int v[],JTextArea a){
+    /*public void Mostrar(int v[],JTextArea a){
         for(int i=0;i<v.length;i++){
             if(i==9||i==18||i==27||i==36||i==45||i==54){
                 a.append(v[i]+"\n");
@@ -41,7 +41,7 @@ public class GenerarCartonLoteria extends JPanel implements MouseListener,MouseM
                 a.append(v[i]+"-");
             }
         }
-    }
+    }*/
     private boolean replay(int tabla[][],int valor){
         for(int i=0;i<4;i++){
            for(int j=0;j<4;j++){
@@ -51,6 +51,10 @@ public class GenerarCartonLoteria extends JPanel implements MouseListener,MouseM
            } 
         }
         return true;
+    }
+    private String generarNombre(int v){
+        String nombre=""+v;
+        return nombre;
     }
     private void generarCarton(){
         for(int i=0;i<4;i++){
@@ -62,22 +66,22 @@ public class GenerarCartonLoteria extends JPanel implements MouseListener,MouseM
                 Carton[i][j]=valorGenerado;
             }
         }
-        car1=new Carta(getClass().getResource("/Imagenes/"+Carton[0][0]+".png"),0,0);
-        car2=new Carta(getClass().getResource("/Imagenes/"+Carton[0][1]+".png"),100,0);
-        car3=new Carta(getClass().getResource("/Imagenes/"+Carton[0][2]+".png"),200,0);
-        car4=new Carta(getClass().getResource("/Imagenes/"+Carton[0][3]+".png"),300,0);
-        car5=new Carta(getClass().getResource("/Imagenes/"+Carton[1][0]+".png"),0,150);
-        car6=new Carta(getClass().getResource("/Imagenes/"+Carton[1][1]+".png"),100,150);
-        car7=new Carta(getClass().getResource("/Imagenes/"+Carton[1][2]+".png"),200,150);
-        car8=new Carta(getClass().getResource("/Imagenes/"+Carton[1][3]+".png"),300,150);
-        car9=new Carta(getClass().getResource("/Imagenes/"+Carton[2][0]+".png"),0,300);
-        car10=new Carta(getClass().getResource("/Imagenes/"+Carton[2][1]+".png"),100,300);
-        car11=new Carta(getClass().getResource("/Imagenes/"+Carton[2][2]+".png"),200,300);
-        car12=new Carta(getClass().getResource("/Imagenes/"+Carton[2][3]+".png"),300,300);
-        car13=new Carta(getClass().getResource("/Imagenes/"+Carton[3][0]+".png"),0,450);
-        car14=new Carta(getClass().getResource("/Imagenes/"+Carton[3][1]+".png"),100,450);
-        car15=new Carta(getClass().getResource("/Imagenes/"+Carton[3][2]+".png"),200,450);
-        car16=new Carta(getClass().getResource("/Imagenes/"+Carton[3][3]+".png"),300,450);
+        car1=new Carta(getClass().getResource("/Imagenes/"+Carton[0][0]+".png"),0,0,this,generarNombre(Carton[0][0]));
+        car2=new Carta(getClass().getResource("/Imagenes/"+Carton[0][1]+".png"),100,0,this,generarNombre(Carton[0][1]));
+        car3=new Carta(getClass().getResource("/Imagenes/"+Carton[0][2]+".png"),200,0,this,generarNombre(Carton[0][2]));
+        car4=new Carta(getClass().getResource("/Imagenes/"+Carton[0][3]+".png"),300,0,this,generarNombre(Carton[0][3]));
+        car5=new Carta(getClass().getResource("/Imagenes/"+Carton[1][0]+".png"),0,150,this,generarNombre(Carton[1][0]));
+        car6=new Carta(getClass().getResource("/Imagenes/"+Carton[1][1]+".png"),100,150,this,generarNombre(Carton[1][1]));
+        car7=new Carta(getClass().getResource("/Imagenes/"+Carton[1][2]+".png"),200,150,this,generarNombre(Carton[1][2]));
+        car8=new Carta(getClass().getResource("/Imagenes/"+Carton[1][3]+".png"),300,150,this,generarNombre(Carton[1][3]));
+        car9=new Carta(getClass().getResource("/Imagenes/"+Carton[2][0]+".png"),0,300,this,generarNombre(Carton[2][0]));
+        car10=new Carta(getClass().getResource("/Imagenes/"+Carton[2][1]+".png"),100,300,this,generarNombre(Carton[2][1]));
+        car11=new Carta(getClass().getResource("/Imagenes/"+Carton[2][2]+".png"),200,300,this,generarNombre(Carton[2][2]));
+        car12=new Carta(getClass().getResource("/Imagenes/"+Carton[2][3]+".png"),300,300,this,generarNombre(Carton[2][3]));
+        car13=new Carta(getClass().getResource("/Imagenes/"+Carton[3][0]+".png"),0,450,this,generarNombre(Carton[3][0]));
+        car14=new Carta(getClass().getResource("/Imagenes/"+Carton[3][1]+".png"),100,450,this,generarNombre(Carton[3][1]));
+        car15=new Carta(getClass().getResource("/Imagenes/"+Carton[3][2]+".png"),200,450,this,generarNombre(Carton[3][2]));
+        car16=new Carta(getClass().getResource("/Imagenes/"+Carton[3][3]+".png"),300,450,this,generarNombre(Carton[3][3]));
     }
     @Override
     public void paint(Graphics g){
